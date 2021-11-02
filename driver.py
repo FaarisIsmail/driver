15ec1b547c10c98abc6e79240eff8a2f360f0bed
FaarisIsmail <63711786+FaarisIsmail@users.noreply.github.com>
2021-11-02 18:05:43 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,9 +1,17 @@
 import webbrowser
 import os
+from kubernetes import client, config
 
 
 
 def main():
+    config.load_kube_config()
+
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
     print("Welcome to Big Data Processing Application\nPlease type the number that corresponds to which application you would like to run:\n1. Apache Hadoop\n2. Apache Spark\n3. Jupyter Notebook\n4. SonarQube and SonarScanner")
     val = int(input("Type the number here > "))
